1ce7a39c83bf8663bd478bc5a55e89dd6df2b461
vishalkudtarkar <vishal.kudtarkar@techsierra.in>
2023-05-13 11:43:09 +0530

    added secret key

--- src/pages/AllUserData/AllUserData.js
+++ src/pages/AllUserData/AllUserData.js
@@ -4,7 +4,23 @@ import server from "../../config/server.json";
 import "./AllUserData.css";
 
 function AllUserData() {
+  const [issercet, setIsSecret] = useState(false);
   const [allUserData, setAllUserData] = useState();
+  const [secretKey, setSecretKey] = useState("");
+
+  const handleOnChange = (e) => {
+    setSecretKey(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (server.secret === secretKey) {
+      setIsSecret(true);
+    } else {
+      return false;
+    }
+  };
+
   useEffect(() => {
     const getAllUserData = async () => {
       try {
@@ -21,22 +37,51 @@ function AllUserData() {
   return (
     <div className="alluser">
       <div className="top-colored"></div>
-        <div className="alluser-heading">
-          <span>Name</span>
-          <span>Email</span>
-          <span>Score</span>
+      {issercet ? (
+        <>
+          <div className="alluser-heading">
+            <span>Name</span>
+            <span>Email</span>
+            <span>Score</span>
+          </div>
+          <div className="scroll">
+            {allUserData?.map((elem, i) => {
+              return (
+                <div className="single-user" key={elem._id}>
+                  {i + 1}.<span> {elem.userName}</span>
+                  <span>{elem.email}</span>
+                  <span>{elem.score} / 10</span>
+                </div>
+              );
+            })}
+          </div>
+          <div className="alluser-mobileview">
+            {allUserData?.map((elem, i) => {
+              return (
+                <div>
+                <strong>{i + 1}.</strong>
+                <div className="alluser-mob" key={elem._id}>
+                  <span>Name : {elem.userName}</span>
+                  <span>Email : {elem.email}</span>
+                  <span>Score : {elem.score} / 10</span>
+                </div>
+                </div>
+              );
+            })}
+          </div>
+        </>
+      ) : (
+        <div className="secret-key">
+          <div>
+            <input
+              type="text"
+              placeholder="Enter the key"
+              onChange={handleOnChange}
+            />
+            <button className="start-btn" onClick={(e) => handleSubmit(e)}>Authenticate</button>
+          </div>
         </div>
-      <div className="scroll">
-        {allUserData?.map((elem, i) => {
-          return (
-            <div className="single-user" key={elem._id}>
-              {i+1}.<span> {elem.userName}</span>
-              <span>{elem.email}</span>
-              <span>{elem.score} / 10</span>
-            </div>
-          );
-        })}
-      </div>
+      )}
     </div>
   );
 }
